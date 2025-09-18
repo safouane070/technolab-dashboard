a879a3c27d2a9aeecee446af1a44126204c777ea
A-Alhemyari <amr@technolableiden.nl>
2025-09-18 14:49:38 +0200

    de nieuw database teo voegen

--- Database/technolab-dashboard.sql
+++ Database/technolab-dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 16 sep 2025 om 12:46
+-- Gegenereerd op: 18 sep 2025 om 14:47
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.1.25
 
@@ -34,20 +34,26 @@ CREATE TABLE `werknemers` (
   `voornaam` varchar(50) NOT NULL,
   `tussenvoegsel` varchar(20) DEFAULT NULL,
   `achternaam` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `werkdag_ma` tinyint(1) DEFAULT 0,
   `werkdag_di` tinyint(1) DEFAULT 0,
+  `werkdag_wo` tinyint(1) DEFAULT 0,
+  `werkdag_do` tinyint(1) DEFAULT 0,
+  `werkdag_vr` tinyint(1) DEFAULT 0,
   `sector` varchar(50) NOT NULL,
   `BHV` tinyint(1) DEFAULT 0,
-  `status` enum('Ziek','Aanwezig','Op de school','Afwezig') DEFAULT 'Aanwezig'
+  `status` enum('Ziek','Aanwezig','Op de school','Afwezig','Eefetjes Afwezig') DEFAULT 'Aanwezig'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `werknemers`
 --
 
-INSERT INTO `werknemers` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `werkdag_ma`, `werkdag_di`, `sector`, `BHV`, `status`) VALUES
-(1, 'Jan', 'van', 'Dijk', 1, 0, 'Techniek', 1, 'Ziek'),
-(2, 'Sophie', NULL, 'Jansen', 1, 1, 'ICT', 0, 'Afwezig');
+INSERT INTO `werknemers` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `email`, `werkdag_ma`, `werkdag_di`, `werkdag_wo`, `werkdag_do`, `werkdag_vr`, `sector`, `BHV`, `status`) VALUES
+(22, 'Anwer', 'de', 'Anwer', 'amr@technolableiden.nl', 1, 1, 1, 1, 1, 'ICT', 0, 'Ziek'),
+(23, 'martijn', 'de', 'martijn', 'amr@technolableiden.nl', 1, 1, 0, 1, 0, 'Onderwijs', 1, 'Aanwezig'),
+(26, 'Amr', '', 'amr', 'amro@gmail.com', 1, 1, 1, 0, 1, 'Techniek', 0, 'Ziek'),
+(27, 'test', '', 'test', 'amro@gmail.com', 1, 1, 1, 1, 1, 'ICT', 1, 'Aanwezig');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -67,7 +73,7 @@ ALTER TABLE `werknemers`
 -- AUTO_INCREMENT voor een tabel `werknemers`
 --
 ALTER TABLE `werknemers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
