2ec888352cfe0bba6d4884e98a243fb094360796
A-Alhemyari <amr@technolableiden.nl>
2025-10-17 11:33:41 +0200

    de databse toegevoegd

--- Database/technolab-dashboard.sql
+++ Database/technolab-dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 29 sep 2025 om 09:45
+-- Gegenereerd op: 17 okt 2025 om 11:33
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.1.25
 
@@ -35,49 +35,55 @@ CREATE TABLE `week_planning` (
   `weeknummer` int(11) NOT NULL,
   `jaar` int(11) NOT NULL,
   `dag` varchar(2) NOT NULL,
-  `status` enum('Aanwezig','Afwezig','Ziek','Op de school','Eefetjes Afwezig') DEFAULT 'Afwezig'
+  `status` enum('Aanwezig','Afwezig','Ziek','Op de school','Eefetjes Afwezig') DEFAULT 'Afwezig',
+  `tijdelijk_tot` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `week_planning`
 --
 
-INSERT INTO `week_planning` (`id`, `werknemer_id`, `weeknummer`, `jaar`, `dag`, `status`) VALUES
-(1, 26, 39, 2025, 'ma', 'Op de school'),
-(2, 26, 39, 2025, 'di', 'Aanwezig'),
-(3, 28, 39, 2025, 'di', 'Op de school'),
-(4, 22, 39, 2025, 'di', 'Eefetjes Afwezig'),
-(5, 23, 39, 2025, 'wo', 'Ziek'),
-(6, 27, 39, 2025, 'wo', 'Op de school'),
-(7, 23, 39, 2025, 'ma', 'Aanwezig'),
-(8, 26, 39, 2025, 'do', 'Aanwezig'),
-(9, 22, 39, 2025, 'do', 'Aanwezig'),
-(10, 23, 39, 2025, 'do', 'Aanwezig'),
-(11, 27, 39, 2025, 'do', 'Aanwezig'),
-(12, 26, 39, 2025, 'vr', 'Op de school'),
-(13, 26, 39, 2025, 'wo', 'Op de school'),
-(14, 26, 40, 2025, 'di', 'Op de school'),
-(15, 26, 40, 2025, 'wo', 'Aanwezig'),
-(16, 22, 40, 2025, 'wo', 'Ziek'),
-(17, 23, 40, 2025, 'di', 'Aanwezig'),
-(18, 26, 39, 2025, 'di', 'Eefetjes Afwezig'),
-(19, 26, 39, 2025, 'di', 'Afwezig'),
-(20, 28, 40, 2025, 'di', 'Ziek'),
-(21, 23, 40, 2025, 'do', 'Op de school'),
-(22, 22, 41, 2025, 'wo', 'Op de school'),
-(23, 28, 39, 2025, 'vr', 'Op de school'),
-(24, 23, 39, 2025, 'di', 'Eefetjes Afwezig'),
-(25, 28, 40, 2025, 'do', 'Op de school'),
-(26, 26, 41, 2025, 'ma', 'Op de school'),
-(27, 28, 41, 2025, 'ma', 'Ziek'),
-(28, 28, 41, 2025, 'di', 'Aanwezig'),
-(29, 26, 39, 2025, 'ma', 'Op de school'),
-(30, 28, 40, 2025, 'ma', 'Ziek'),
-(31, 28, 40, 2025, 'di', 'Aanwezig'),
-(32, 26, 40, 2025, 'do', 'Afwezig'),
-(33, 22, 40, 2025, 'di', 'Op de school'),
-(34, 26, 39, 2025, 'di', 'Op de school'),
-(35, 27, 40, 2025, 'ma', 'Eefetjes Afwezig');
+INSERT INTO `week_planning` (`id`, `werknemer_id`, `weeknummer`, `jaar`, `dag`, `status`, `tijdelijk_tot`) VALUES
+(39, 29, 40, 2025, 'di', 'Op de school', NULL),
+(42, 29, 40, 2025, 'vr', 'Op de school', NULL),
+(47, 29, 40, 2025, 'do', 'Op de school', NULL),
+(240, 29, 41, 2025, 'ma', 'Op de school', NULL),
+(304, 29, 41, 2025, 'di', 'Op de school', NULL),
+(353, 29, 41, 2025, 'we', 'Eefetjes Afwezig', NULL),
+(375, 29, 41, 2025, 'do', 'Eefetjes Afwezig', NULL),
+(400, 29, 41, 2025, 'wo', 'Eefetjes Afwezig', NULL),
+(443, 29, 42, 2025, 'di', 'Ziek', NULL),
+(449, 29, 43, 2025, 'ma', 'Afwezig', NULL),
+(451, 29, 51, 2025, 'ma', 'Afwezig', NULL),
+(460, 29, 41, 2025, 'vr', 'Aanwezig', NULL),
+(473, 29, 42, 2025, 'ma', 'Ziek', NULL),
+(581, 29, 42, 2025, 'wo', 'Aanwezig', NULL),
+(585, 29, 42, 2025, 'vr', 'Afwezig', NULL),
+(640, 32, 42, 2025, 'wo', 'Aanwezig', NULL),
+(642, 32, 42, 2025, 'do', 'Afwezig', NULL),
+(644, 32, 42, 2025, 'vr', 'Eefetjes Afwezig', NULL),
+(663, 37, 42, 2025, 'wo', 'Afwezig', NULL),
+(664, 38, 42, 2025, 'wo', 'Afwezig', NULL),
+(665, 38, 42, 2025, 'do', 'Afwezig', NULL),
+(666, 37, 42, 2025, 'do', 'Afwezig', NULL),
+(669, 38, 42, 2025, 'vr', 'Eefetjes Afwezig', NULL),
+(670, 37, 42, 2025, 'vr', 'Ziek', NULL),
+(677, 38, 42, 2025, 'di', 'Eefetjes Afwezig', NULL),
+(679, 32, 42, 2025, 'di', 'Eefetjes Afwezig', NULL),
+(680, 37, 42, 2025, 'di', 'Eefetjes Afwezig', NULL),
+(682, 38, 42, 2025, 'ma', 'Ziek', NULL),
+(683, 32, 42, 2025, 'ma', 'Ziek', NULL),
+(684, 37, 42, 2025, 'ma', 'Ziek', NULL),
+(686, 32, 44, 2025, 'ma', 'Aanwezig', NULL),
+(705, 29, 42, 2025, 'do', 'Afwezig', NULL),
+(712, 40, 42, 2025, 'vr', 'Aanwezig', NULL),
+(713, 41, 42, 2025, 'vr', 'Afwezig', NULL),
+(715, 40, 42, 2025, 'ma', 'Aanwezig', NULL),
+(716, 41, 42, 2025, 'ma', 'Aanwezig', NULL),
+(717, 40, 42, 2025, 'di', 'Aanwezig', NULL),
+(718, 41, 42, 2025, 'di', 'Afwezig', NULL),
+(719, 40, 42, 2025, 'wo', 'Aanwezig', NULL),
+(720, 41, 42, 2025, 'wo', 'Afwezig', NULL);
 
 -- --------------------------------------------------------
 
@@ -112,11 +118,12 @@ CREATE TABLE `werknemers` (
 --
 
 INSERT INTO `werknemers` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `email`, `werkdag_ma`, `werkdag_di`, `werkdag_wo`, `werkdag_do`, `werkdag_vr`, `sector`, `BHV`, `status`, `status_ma`, `status_di`, `status_wo`, `status_do`, `status_vr`, `tijdelijk_tot`) VALUES
-(22, 'Anwer', 'de', 'Anwer', 'amr@technolableiden.nl', 1, 1, 1, 1, 0, 'ICT', 0, 'Aanwezig', 'Aanwezig', 'Afwezig', 'Op de school', 'Afwezig', 'Eefetjes Afwezig', NULL),
-(23, 'martijn', 'de', 'martijn', 'amr@technolableiden.nl', 1, 1, 0, 1, 0, 'Onderwijs', 1, 'Aanwezig', 'Aanwezig', 'Afwezig', 'Op de school', 'Afwezig', 'Afwezig', NULL),
-(26, 'Amr', '', 'amr', 'amro@gmail.com', 1, 1, 1, 0, 1, 'Techniek', 0, 'Eefetjes Afwezig', 'Ziek', 'Ziek', 'Aanwezig', 'Afwezig', 'Afwezig', '0000-00-00 00:00:00'),
-(27, 'test', '', 'test', 'amro@gmail.com', 1, 1, 1, 1, 1, 'ICT', 1, 'Ziek', 'Eefetjes Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL),
-(28, 'Amr', '', 'Anwer', 'amro@gmail.com', 0, 1, 1, 1, 0, 'ICT', 1, 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL);
+(29, 'martijn', 'de', 'amr', 'amro@gmail.com', 1, 1, 1, 0, 1, 'ICT', 1, 'Eefetjes Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL),
+(32, 'Robert', '', 'Commerell', 'amr@technolableiden.nl', 0, 1, 1, 1, 1, 'ICT', 1, 'Aanwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL),
+(37, 'martijn', '', 'martijn', 'amr@technolableiden.nl', 1, 1, 0, 1, 1, 'ICT', 0, 'Aanwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL),
+(38, 'martijn', '', 'Anwer', 'amr@technolableiden.nl', 1, 1, 0, 1, 1, 'ICT', 0, 'Aanwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL),
+(40, 'Amr', '', 'amr', 'amro@gmail.com', 1, 1, 1, 1, 0, 'ICT', 1, 'Aanwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL),
+(41, 'Anwer', '', 'Commerell', 'amr@technolableiden.nl', 1, 0, 0, 0, 0, 'ICT', 1, 'Aanwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', 'Afwezig', NULL);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -127,7 +134,7 @@ INSERT INTO `werknemers` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `emai
 --
 ALTER TABLE `week_planning`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `werknemer_id` (`werknemer_id`);
+  ADD UNIQUE KEY `uniq_planning` (`werknemer_id`,`weeknummer`,`jaar`,`dag`);
 
 --
 -- Indexen voor tabel `werknemers`
@@ -143,13 +150,13 @@ ALTER TABLE `werknemers`
 -- AUTO_INCREMENT voor een tabel `week_planning`
 --
 ALTER TABLE `week_planning`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=721;
 
 --
 -- AUTO_INCREMENT voor een tabel `werknemers`
 --
 ALTER TABLE `werknemers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
